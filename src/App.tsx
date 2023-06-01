e15042e3c09b9f672a8c2c037e4dbef3e7a5bc0e
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2023-06-01 18:02:42 -0300

    feat: added typography and global colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
-
-function App() {
+export const App = () => {
   return (
     <>
-      
+      <div className="flex justify-center items-center w-screen h-screen bg-brand-2">
+        <span className="text-grey-1 text-heading-4-600 bg-brand-3 p-5">
+          Teste
+        </span>
+      </div>
     </>
-  )
-}
-
-export default App
+  );
+};
