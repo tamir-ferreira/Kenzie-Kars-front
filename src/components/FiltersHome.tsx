5bc4ee2dbff1ca9c813c02fb4c319b45c578ce1a
AntonioAugustoRezende <antoniorezende7@outlook.com>
2023-06-05 20:31:23 -0300

    feat: create aside

--- src/components/FiltersHome.tsx
+++ src/components/FiltersHome.tsx
@@ -1,20 +1,171 @@
-interface FilterProps {
-  title: string;
-}
+import { useState } from "react";
+import { mockFilter } from "../mocks/filter";
 
-export const FilterHome = ({ title }: FilterProps) => {
+export const FilterHome = () => {
+  const [brand, setBrand] = useState<null | string>();
+  const [model, setModel] = useState<null | string>();
+  const [color, setColor] = useState<null | string>();
+  const [year, setYear] = useState<null | string>();
+  const [fuel, setFuel] = useState<null | string>();
+  const [km, setKm] = useState<null | string>();
+  const models = mockFilter.filter((model) => model.brand === brand);
+  const colors = models.filter((models) => models.model === model);
+  const years = colors.filter((models) => models.color === color);
+  const combs = years.filter((models) => models.year === year);
+  const kilomers = combs.filter((models) => models.fuel === fuel);
+
+  const minKm =
+    km === "min" && kilomers.sort((a, b) => a.kilometers - b.kilometers);
+
+  const maxKm =
+    km === "max" && kilomers.sort((a, b) => a.kilometers + b.kilometers);
+
   return (
-    <div className="mb-10">
-      <h2 className="text-heading-4-600 mb-4 ml-8">{title}</h2>
-      <ul className="ml-10">
-        {/* Fazer map dos modelos */}
-        <li className="cursor-pointer text-heading-6-500 text-grey-3">
-          General Motors
-        </li>
-        <li className="cursor-pointer text-heading-6-500 text-grey-3">Fiat</li>
-        <li className="cursor-pointer text-heading-6-500 text-grey-3">Ford</li>
-        <li className="cursor-pointer text-heading-6-500 text-grey-3">Honda</li>
-      </ul>
-    </div>
+    <aside>
+      <div className="mb-10">
+        <h2 className="text-heading-4-600 mb-4 ml-8">Marca</h2>
+        <ul className="ml-10">
+          {mockFilter.map((cars, index) => {
+            return (
+              <li
+                key={index}
+                className="cursor-pointer text-heading-6-500 text-grey-3"
+                onClick={() => setBrand(cars.brand)}
+              >
+                {cars.brand}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="mb-10">
+        <h2 className="text-heading-4-600 mb-4 ml-8">Modelo</h2>
+        <ul className="ml-10">
+          {models.length
+            ? models.map((cars, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-heading-6-500 text-grey-3"
+                    onClick={() => setModel(cars.model)}
+                  >
+                    {cars.model}
+                  </li>
+                );
+              })
+            : mockFilter.map((cars, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-heading-6-500 text-grey-3"
+                    onClick={() => setModel(cars.model)}
+                  >
+                    {cars.model}
+                  </li>
+                );
+              })}
+        </ul>
+      </div>
+      <div className="mb-10">
+        <h2 className="text-heading-4-600 mb-4 ml-8">Cor</h2>
+        <ul className="ml-10">
+          {colors.length
+            ? colors.map((cars, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-heading-6-500 text-grey-3"
+                    onClick={() => setColor(cars.color)}
+                  >
+                    {cars.color}
+                  </li>
+                );
+              })
+            : mockFilter.map((cars, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-heading-6-500 text-grey-3"
+                    onClick={() => setColor(cars.color)}
+                  >
+                    {cars.color}
+                  </li>
+                );
+              })}
+        </ul>
+      </div>
+      <div className="mb-10">
+        <h2 className="text-heading-4-600 mb-4 ml-8">Ano</h2>
+        <ul className="ml-10">
+          {years.length
+            ? years.map((cars, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-heading-6-500 text-grey-3"
+                    onClick={() => setYear(cars.year)}
+                  >
+                    {cars.year}
+                  </li>
+                );
+              })
+            : mockFilter.map((cars, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-heading-6-500 text-grey-3"
+                    onClick={() => setYear(cars.year)}
+                  >
+                    {cars.year}
+                  </li>
+                );
+              })}
+        </ul>
+      </div>
+      <div className="mb-10">
+        <h2 className="text-heading-4-600 mb-4 ml-8">Combustível</h2>
+        <ul className="ml-10">
+          {combs.length
+            ? combs.map((cars, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-heading-6-500 text-grey-3"
+                    onClick={() => setFuel(cars.fuel)}
+                  >
+                    {cars.fuel}
+                  </li>
+                );
+              })
+            : mockFilter.map((cars, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-heading-6-500 text-grey-3"
+                    onClick={() => setFuel(cars.fuel)}
+                  >
+                    {cars.fuel}
+                  </li>
+                );
+              })}
+        </ul>
+      </div>
+      <div className="mb-10">
+        <h2 className="text-heading-4-600 mb-4 ml-8">KM</h2>
+        <div>
+          <button className="bg-grey-3 btn-" onClick={() => setKm("min")}>
+            Minimo
+          </button>
+          <button onClick={() => setKm("max")}>Maximo</button>
+        </div>
+      </div>
+      <div className="mb-10">
+        <h2 className="text-heading-4-600 mb-4 ml-8">Preço</h2>
+        <div>
+          <button>Minimo</button>
+          <button>Maximo</button>
+        </div>
+      </div>
+    </aside>
   );
 };
---
+++ src/mocks/filter.ts
@@ -0,0 +1,68 @@
+export const mockFilter = [
+  {
+    model: "Corola",
+    price: "500.000,00",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    brand: "Toyota",
+    year: "2021",
+    fuel: "Eletric",
+    color: "Azul",
+    kilometers: 500,
+  },
+  {
+    model: "Corola",
+    price: "500.000,00",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    brand: "Toyota",
+    year: "2021",
+    fuel: "Eletric",
+    color: "Azul",
+    kilometers: 50000,
+  },
+  {
+    model: "Corola",
+    price: "500.000,00",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    brand: "Toyota",
+    year: "2021",
+    fuel: "Eletric",
+    color: "Azul",
+    kilometers: 100000,
+  },
+  {
+    model: "M3",
+    price: "200.000,00",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    brand: "BMW",
+    year: "2021",
+    fuel: "Alcool",
+    color: "Black",
+    kilometers: 2000000,
+  },
+  {
+    model: "Camaro",
+    price: "300.000,00",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    brand: "GM",
+    year: "2021",
+    fuel: "Gasolina",
+    color: "Branco",
+    kilometers: 30000,
+  },
+  {
+    model: "C3",
+    price: "500.000,00",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    brand: "Citroe",
+    year: "2021",
+    fuel: "Eletrico",
+    color: "Azul",
+    kilometers: 40,
+  },
+];
