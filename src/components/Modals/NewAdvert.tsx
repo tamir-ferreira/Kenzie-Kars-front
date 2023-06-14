4b9f051eb48408d9587fa4ae92c6ec950c0f84f2
Tamir Ferreira <bigtam_rj@yahoo.com.br>
2023-06-14 19:51:22 -0300

    feat: start new advert(logic Kenzie API/Selects)

--- src/components/Modals/NewAdvert.tsx
+++ src/components/Modals/NewAdvert.tsx
@@ -1,10 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../Button";
 import { Input } from "../Input";
 import { TextArea } from "../TextArea";
+import { Select } from "../Select";
+import { getBrands, getModelsByBrand } from "../../services/requests";
+
+// interface Model {
+//   name: string;
+// }
+
+interface Model {
+  id?: string;
+  brand?: string;
+  name?: string;
+  fuel?: number;
+  value?: number;
+  year?: string;
+}
 
 export const NewAdvert = () => {
   const [inputs, setInputs] = useState<string[]>([]);
+  const [brands, setBrands] = useState<string[]>([]);
+  const [selectedBrand, setSelectedBrand] = useState("");
+  const [models, setModels] = useState<Model[]>([]);
+  const [selectedModel, setSelectedModel] = useState<Model>();
+
+  useEffect(() => {
+    const loadCars = async () => {
+      const cars = await getBrands();
+      const carBrands = Object.keys(cars);
+      setBrands(carBrands);
+      // console.log(cars);
+    };
+
+    loadCars();
+  }, []);
+
+  const updateSelectedBrand = async (brand: string) => {
+    setSelectedBrand(brand);
+    const findModels = await getModelsByBrand(brand);
+
+    setModels(findModels);
+    // console.log(findModels);
+  };
+
+  const updateSelectedModel = async (model: Model) => {
+    // console.log(model);
+    // console.log(models);
+    const findModel = models.find((car) => {
+      return car.name == model;
+    });
+    setSelectedModel(findModel);
+
+    // console.log(findModel);
+  };
 
   const addInput = (): void => {
     if (inputs.length <= 5) {
@@ -15,11 +64,45 @@ export const NewAdvert = () => {
   return (
     <>
       <h3 className="text-body-2-500 mb-6">Informações do veículo</h3>
-      <Input label="Marca" placeholder="digite a marca" type="text"></Input>
-      <Input label="Modelo" placeholder="digite o modelo" type="text"></Input>
+      <Select label="Marca" name="marca" handleSelect={updateSelectedBrand}>
+        <>
+          {brands.map((brand, index) => {
+            return (
+              <option key={index} value={brand}>
+                {brand.charAt(0).toUpperCase() + brand.slice(1)}
+              </option>
+            );
+          })}
+        </>
+      </Select>
+      <Select
+        label="Modelo"
+        name="modelo"
+        handleSelect={updateSelectedModel}
+        disabled={!models.length}
+      >
+        <>
+          {/* <option value="teste">teste</option>; */}
+          {models &&
+            models.map((model, index) => {
+              return (
+                <option key={index} value={model.name}>
+                  {model.name.charAt(0).toUpperCase() + model.name.slice(1)}
+                </option>
+              );
+            })}
+        </>
+      </Select>
       <div className="flex gap-3.5">
-        <Input label="Ano" placeholder="digite o ano" type="text"></Input>
-        <Input label="Combustível" placeholder="digite o combustível" type="text"></Input>
+        <Input label="Ano" type="text" value={selectedModel && selectedModel.year} disabled></Input>
+        <Input
+          label="Combustível"
+          type="text"
+          value={
+            selectedModel &&
+            (selectedModel.fuel === 1 ? "Flex" : selectedModel.fuel === 2 ? "Elétrico" : "Hibrido")
+          }
+        ></Input>
       </div>
       <div className="flex gap-3.5">
         <Input label="Quilometragem" placeholder="digite a quilometragem" type="text"></Input>
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,35 +1,48 @@
 import { ReactNode } from "react";
 
 interface iSelectProps {
-  id: string;
   label: string;
   name: string;
+  disabled?: boolean;
   children: ReactNode;
+  handleSelect?: (name: string) => void | Promise<void>;
   error?: string;
 }
 
-export const Select = ({ id, label, name, children, error }: iSelectProps) => {
+export const Select = ({
+  label,
+  name,
+  disabled = false,
+  children,
+  handleSelect,
+  error,
+}: iSelectProps) => {
+  console.log(disabled);
   return (
     <div className="flex flex-col gap-2 input-width">
-      <label
-        className="font-inter font-medium text-sm text-grey-1"
-        htmlFor={id}>
+      <label className="font-inter font-medium text-sm text-grey-1" htmlFor={label}>
         {label}
       </label>
 
       <select
-        id={id}
+        id={label}
         name={name}
-        className="w-full h-12 text-grey-3 px-4 border-grey-8 border-1.5 rounded bg-white-fixed hover:bg-grey-8 mb-6">
-        <option value="" hidden={true}>
-          Escolha uma opção
-        </option>
+        className="w-full h-12 text-grey-3 px-4 border-grey-8 border-1.5 rounded bg-white-fixed hover:bg-grey-8 mb-6"
+        onChange={(event) => handleSelect && handleSelect(event.target.value)}
+        disabled={disabled}
+      >
+        {!disabled && (
+          <option value="" hidden={true}>
+            Escolha uma opção
+          </option>
+        )}
         {children}
       </select>
       {error && (
         <p
           className="flex self-end text-alert-1 text-input-error absolute bottom-[-20px] "
-          aria-label="erro na validação do campo">
+          aria-label="erro na validação do campo"
+        >
           {error}
         </p>
       )}
