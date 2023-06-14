9c52118eecaf8ed87d8a6a76f33715ebb898dc2d
AntonioAugustoRezende <antoniorezende7@outlook.com>
2023-06-14 12:47:33 -0300

    feat: eslint

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -15,7 +15,10 @@ export const TextArea = ({
 }: iTextAreaProps) => {
   return (
     <div className="flex flex-col gap-2 input-width">
-      <label className="font-inter font-medium text-sm text-grey-1" htmlFor={label}>
+      <label
+        className="font-inter font-medium text-sm text-grey-1"
+        htmlFor={label}
+      >
         {label}
       </label>
       <textarea
