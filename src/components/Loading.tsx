1443cab7a9700482723480e52e8c8cd0fc340d7b
AntonioAugustoRezende <antoniorezende7@outlook.com>
2023-06-14 14:57:24 -0300

    feat: create efect loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,9 +2,9 @@ export const Loading = () => {
   return (
     <div className="fixed top-0 bg-black/50 w-screen h-screen flex justify-center items-center left-0">
       <div className="flex gap-[10px]">
-        <div className="h-5 w-5 bg-brand-1 rounded-full animation-loading"></div>
-        <div className="animation-delay-1"></div>
-        <div className="animation-delay-2"></div>
+        <div className="h-5 w-5 bg-brand-1 rounded-full animate-bounce"></div>
+        <div className="animation-delay-1 h-5 w-5 bg-brand-1 rounded-full animate-bounce"></div>
+        <div className="animation-delay-2 h-5 w-5 bg-brand-1 rounded-full animate-bounce"></div>
       </div>
     </div>
   );
