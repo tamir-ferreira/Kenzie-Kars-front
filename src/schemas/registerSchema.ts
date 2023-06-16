6ab35b86de9881f62d89937cd2195fc16936de29
Tamir Ferreira <bigtam_rj@yahoo.com.br>
2023-06-16 12:05:10 -0300

    fix: update cpf field in register schema

--- src/schemas/registerSchema.ts
+++ src/schemas/registerSchema.ts
@@ -3,13 +3,11 @@ import { z } from "zod";
 export const registerSchema = z
   .object({
     name: z.string().nonempty("O nome é obrigatório"),
-    email: z
-      .string()
-      .nonempty("O email é obrigatório")
-      .email("E-mail inválido"),
+    email: z.string().nonempty("O email é obrigatório").email("E-mail inválido"),
     cpf: z
       .string()
       .nonempty("O CPF é obrigatório")
+      .min(11, "O CPF deve conter no mínimo 11 digitos")
       .max(14, "O CPF deve conter no máximo 14 digitos"),
     phone: z.string().nonempty("O telefone é obrigatório"),
     birthdate: z.string().nonempty("A data de nascimento é obrigatória"),
