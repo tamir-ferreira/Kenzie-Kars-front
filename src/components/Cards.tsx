d69d9ba60f0f4c628265d30a836870488bbf82fc
lucana-m <lucana_monica@hotmail.com>
2023-06-19 22:20:33 -0300

    Fix Cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -45,10 +45,15 @@ export const Cards = ({ car }: iAdverts) => {
   return (
     <Link to={`/product/${car.id}`}>
       <li
-        className="flex flex-col items-start pt-0 min-h-[350px] min-w-[312px] w-[312px] group mb-9 cursor-pointer"
-        onClick={() => setAdvert(car)}>
-        <div className="flex justify-center items-center bg-grey-7 w-full mb-4 relative border-2 border-transparent group-hover:border-brand-1 group-hover:border-solid ">
-          <img src={car.cover_image} alt="carro" className="" />
+        className="flex flex-col justify-between items-start pt-0 h-[356px] w-[312px] group mb-9 cursor-pointer"
+        onClick={() => setAdvert(car)}
+      >
+        <div className="flex justify-center items-center bg-grey-7 w-full h-[152px] relative border-2 border-transparent group-hover:border-brand-1 group-hover:border-solid ">
+          <img
+            src={car.cover_image}
+            alt="carro"
+            className="object-contain w-full h-full"
+          />
           {!isSeller && (
             <>
               {discount && (
@@ -72,43 +77,45 @@ export const Cards = ({ car }: iAdverts) => {
             </>
           )}
         </div>
-        <div className="flex mb-4">
-          <h2 className="font-lexend text-base font-semibold">{`${car.brand} - ${car.model}`}</h2>
-        </div>
-        <p className="ellipsis-limiter text-grey-2 text-sm text-body-2-400 leading-6 text-start mb-4 max-w-full">
-          {car.description}
-        </p>
-        <div className="flex items-center mb-4">
-          {!isSeller && (
-            <>
-              <UserInitials name={car.user.name} />
-              <span className="ml-2 font-medium text-sm text-grey-2">
-                {car.user.name}
+        <div className="h-3/6 w-full flex flex-col justify-between">
+          <div className="flex">
+            <h2 className="font-lexend text-base font-semibold">{`${car.brand} - ${car.model}`}</h2>
+          </div>
+          <p className="ellipsis-limiter text-grey-2 text-sm text-body-2-400 leading-6 text-start max-w-full">
+            {car.description}
+          </p>
+          <div className="flex items-center">
+            {!isSeller && (
+              <>
+                <UserInitials name={car.user.name} />
+                <span className="ml-2 font-medium text-sm text-grey-2">
+                  {car.user.name}
+                </span>
+              </>
+            )}
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center ">
+              <span className="bg-brand-4 text-brand-1 font-medium text-sm px-2 py-1 rounded">
+                {`${"3000000"} KM`}
               </span>
-            </>
+              <span className="bg-brand-4 text-brand-1 font-medium text-sm px-2 py-1 rounded ml-3">
+                {car.year}
+              </span>
+            </div>
+            <span className="font-lexend text-grey-1 font-medium text-base">{`R$ ${car.price}`}</span>
+          </div>
+          {isSeller && (
+            <div className="flex gap-4 mt-4">
+              <Button btnSize="btn-medium" btnColor="btn-outline-1">
+                Editar
+              </Button>
+              <Button btnSize="btn-medium" btnColor="btn-outline-1">
+                Ver detalhes
+              </Button>
+            </div>
           )}
         </div>
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center ">
-            <span className="bg-brand-4 text-brand-1 font-medium text-sm px-2 py-1 rounded">
-              {`${"3000000"} KM`}
-            </span>
-            <span className="bg-brand-4 text-brand-1 font-medium text-sm px-2 py-1 rounded ml-3">
-              {car.year}
-            </span>
-          </div>
-          <span className="font-lexend text-grey-1 font-medium text-base">{`R$ ${car.price}`}</span>
-        </div>
-        {isSeller && (
-          <div className="flex gap-4 mt-4">
-            <Button btnSize="btn-medium" btnColor="btn-outline-1">
-              Editar
-            </Button>
-            <Button btnSize="btn-medium" btnColor="btn-outline-1">
-              Ver detalhes
-            </Button>
-          </div>
-        )}
       </li>
     </Link>
   );
